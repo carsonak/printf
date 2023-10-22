7f83df2e06c3a21ceffaff787e408f68ae5d026a
Andrew <49104171+carsonak@users.noreply.github.com>
2023-10-22 15:52:52 +0300

    added print_binary test file

--- print_binary.c
+++ print_binary.c
@@ -10,37 +10,32 @@
  */
 long int print_b(va_list args, char *buf, long int *buf_i)
 {
-	long int nob = 0, num = 0, i = 0;
-	unsigned long int num_cpy = 0, hlp = 1;
+	long int nob = 0, count = 0;
+	unsigned long int num = 0, hlp = 2;
 
 	if (args)
 	{
-		num = va_arg(args, long int);
-		num_cpy = (num < 0 ? -num : num);
-
+		num = va_arg(args, unsigned long int);
 		if (num == 0)
 		{
 			buf[*buf_i] = '0';
 			return (0);
 		}
-		while (hlp < num_cpy)
+		while (hlp < num)
 		{
 			hlp *= 2;
-			i++;
+			count++;
 		}
 
-		if (hlp > num_cpy)
-			i--;
-
-		hlp = i;
-		if ((*buf_i + i) >= PRINTF_BUFFER - 24)
+		hlp = count;
+		if ((*buf_i + count) >= PRINTF_BUFFER - 24)
 			nob += _flushbuff(buf, buf_i);
 
-		while (i >= 0)
+		while (count >= 0)
 		{
-			buf[*buf_i + i] = (num_cpy % 2) + '0';
-			num_cpy /= 2;
-			i--;
+			buf[*buf_i + count] = (num % 2) + '0';
+			num /= 2;
+			count--;
 		}
 
 		*buf_i += hlp;
